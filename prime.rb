564bb01516d386d727baa62248e53c767493db48
Vic Lam <lam.vic.k@gmail.com>
2016-11-25 18:36:41 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(numero)
+  if numero.abs <= 1
+    primo = false
+  else
+    primo = true
+    a=*(2..(numero.abs/2))
+    a.each do |x|
+      if numero % x == 0
+        primo = false
+      end
+    end
+  end
+  return primo
+end
